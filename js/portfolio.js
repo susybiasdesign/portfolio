531985bb0d1ac5a6b8604d4cde6507310dd3ab86
susybiasdesign <susy@susybiasdesign.com>
2017-04-24 12:03:49 -0700

    the modals are working most of the time

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,45 +4,60 @@ $(document).ready(function() {
 
 
 
-
-
 // Get the modal
 
-var modal = document.getElementById('modalOpen');
+        var modalparent = document.getElementsByClassName("modal_multi");
 
-// Get the button that opens the modal
+        // Get the button that opens the modal
 
-var btn = document.getElementById("modalButton");
+        var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
 
-// Get the <span> element that closes the modal
+        // Get the <span> element that closes the modal
+        var span_close_multi = document.getElementsByClassName("close_multi");
 
-var span = document.getElementsByClassName("close")[0];
+        // When the user clicks the button, open the modal
+        function setDataIndex() {
 
-// When the user clicks the button, open the modal 
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+            for (i = 0; i < modal_btn_multi.length; i++)
+            {
+                modal_btn_multi[i].setAttribute('data-index', i);
+                modalparent[i].setAttribute('data-index', i);
+                span_close_multi[i].setAttribute('data-index', i);
+            }
+        }
 
 
 
+        for (i = 0; i < modal_btn_multi.length; i++)
+        {
+            modal_btn_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.display = "block";
+            };
 
+            // When the user clicks on <span> (x), close the modal
+            span_close_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.display = "none";
+            };
 
+        }
 
+        window.onload = function() {
+
+            setDataIndex();
+        };
+
+        window.onclick = function(event) {
+            if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+                modalparent[event.target.getAttribute('data-index')].style.display = "none";
+            }
+
+            // OLD CODE
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        };
 
 
 
